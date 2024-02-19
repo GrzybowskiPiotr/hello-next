a72881b6bceceeb6f1c3c7a542c01ae621edc2d8
GrzybowskiPiotr <grzybowski_piotr@hotmail.com>
2024-02-19 13:13:39 +0100

    photo

--- src/app/Photo/page.js
+++ src/app/Photo/page.js
@@ -2,5 +2,5 @@ import Image from "next/image";
 import summersale from "/public/summersale.jpg";
 
 export default function Photo() {
-	return <Image src={summersale}  placeholder="blur"/>;
+	return <Image src={summersale} placeholder="blur" />;
 }
